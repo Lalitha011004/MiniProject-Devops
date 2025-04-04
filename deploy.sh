13dd599b728668b9162df6e759d53da8ee72dde2
Lalitha011004 <127740404+Lalitha011004@users.noreply.github.com>
2025-04-05 04:47:17 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
     echo Hello
-    sh 'chmod +x build.sh'
+    chmod +x build.sh
     sh './build.sh'
     docker login -u lalithambigai -p Lali_0121
     docker tag test lalithambigai011004/miniproject-devops
